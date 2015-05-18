d7089431c8166acd4343111e551b74e38663db92
Andrew Volchek <andrew.volchek@gmail.com>
2015-05-18 22:28:51 +0300

    Added hash-table directory

---
+++ hash-table/MichaelHashTable.h
@@ -0,0 +1,17 @@
+//
+// Created by andrew on 18.5.15.
+//
+
+#ifndef LOCKFREE_BENCH_MICHAELHASHTABLE_H
+#define LOCKFREE_BENCH_MICHAELHASHTABLE_H
+
+#include "../list-based-set/LockFreeListSetWithPool.h"
+#include "../hp/HP.h"
+
+template< typename Item, typename ListSet = LockFreeListSetWithPool<Item, HP>>
+class MichaelHashTable {
+
+};
+
+
+#endif //LOCKFREE_BENCH_MICHAELHASHTABLE_H
