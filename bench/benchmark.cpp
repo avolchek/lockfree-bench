cd1628ae339f3279b976ae1435f186935bd19987
Andrew Volchek <andrew.volchek@gmail.com>
2015-05-22 23:59:33 +0300

    .

--- bench/benchmark.cpp
+++ bench/benchmark.cpp
@@ -111,10 +111,10 @@ void workerListProc(Container *c, int *operationsCount) {
     while (isRunning.load(std::memory_order_relaxed)) {
         int op = rand() % 100;
         int x = rand() % 20000;
-        if (op < 40) {
+        if (op < 5) {
             //std::cerr << "add" << std::endl;
             c->add(x);
-        } else if (op < 80) {
+        } else if (op < 10) {
             //std::cerr << "remove" << std::endl;
             c->remove(x);
         } else {
